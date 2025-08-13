0955094dc81f41504dde09d97630d6d696f4b0e4
heleno2307 <igor.campos@rochapecas.com.br>
2025-08-13 15:45:58 -0300

    feat: implement DeleteQuestion use case and related tests, update in-memory repository methods

---
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -0,0 +1,31 @@
+import type { QuestionsRepository } from "../repositories/questions-repository.js";
+
+interface DeleteQuestionUseCaseRequest {
+  questionId: string;
+  authorId: string
+}
+
+interface DeleteQuestionUseCaseResponse {}
+
+export class DeleteQuestionUseCase {
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    questionId,
+    authorId
+  }: DeleteQuestionUseCaseRequest): Promise<DeleteQuestionUseCaseResponse> {
+    const question = await this.questionsRepository.findById(questionId);
+
+    if (!question) {
+      throw new Error("Question not found");
+    }
+
+    if (question.authorId.toString() !== authorId) {
+      throw new Error("Unauthorized");
+    }
+
+    await this.questionsRepository.delete(question);
+
+    return {};
+  }
+}
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -2,14 +2,32 @@ import type { QuestionsRepository } from "@/domain/forum/application/repositorie
 import type { Question } from "@/domain/forum/enterprise/entities/question.js";
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
-    public items: Question[] = [];
+  public items: Question[] = [];
 
-    async create(question: Question): Promise<void> {
-        this.items.push(question);
+  async findById(id: string): Promise<Question | null> {
+    const question = this.items.find((item) => item.id.toString() === id);
+
+    if (!question) {
+      return null;
     }
 
-    async findBySlug(slug: string): Promise<Question | null> {
-        const question = this.items.find((item) => item.slug.value === slug);
-        return question || null;
+    return question;
+  }
+
+  async findBySlug(slug: string): Promise<Question | null> {
+    const question = this.items.find((item) => item.slug.value === slug);
+    return question || null;
+  }
+
+  async create(question: Question): Promise<void> {
+    this.items.push(question);
+  }
+
+  async delete(question: Question): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id);
+
+    if (itemIndex !== -1) {
+      this.items.splice(itemIndex, 1);
     }
+  }
 }
