a21bf501417f8e4285192f13951a1a2706f13699
heleno2307 <igor.campos@rochapecas.com.br>
2025-08-15 11:12:41 -0300

    feat: implement CommentOnQuestion use case with in-memory repository and tests

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,40 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository.js'
+import { CommentOnQuestionUseCase } from './comment-on-question.js'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments.js'
+import { makeQuestion } from 'test/factories/make-question.js'
+
+// Repositório em memória para simular o banco de dados
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let sut: CommentOnQuestionUseCase
+
+describe('Create Question Comments', () => {
+  // Antes de cada teste instanciamos o repositório
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    )
+  })
+
+  // system under test sut
+
+  it('should be able to create a question', async () => {
+    const question = makeQuestion()
+    inMemoryQuestionsRepository.create(question)
+
+    await sut.execute({
+      content: 'Novo comentário',
+      authorId: question.authorId.toString(),
+      questionId: question.id.toString(),
+    })
+
+    expect(inMemoryQuestionCommentsRepository.items[0]?.content).toEqual(
+      'Novo comentário',
+    )
+  })
+})
---
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -0,0 +1,43 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id.js'
+import { QuestionComment } from '../../enterprise/entities/question-comment.js'
+import type { QuestionCommentsRepository } from '../repositories/question-comments-repository.js'
+import type { QuestionsRepository } from '../repositories/questions-repository.js'
+
+interface CommentOnQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+  content: string
+}
+
+interface CommentOnQuestionUseCaseResponse {
+  questionComment: QuestionComment
+}
+
+export class CommentOnQuestionUseCase {
+  constructor(
+    private questionRepository: QuestionsRepository,
+    private questionCommentsRepository: QuestionCommentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    content,
+    questionId,
+  }: CommentOnQuestionUseCaseRequest): Promise<CommentOnQuestionUseCaseResponse> {
+    const question = await this.questionRepository.findById(questionId)
+
+    if (!question) {
+      throw new Error('Question not found')
+    }
+
+    const questionComment = QuestionComment.create({
+      authorId: new UniqueEntityID(authorId),
+      questionId: new UniqueEntityID(questionId),
+      content,
+    })
+
+    await this.questionCommentsRepository.create(questionComment)
+
+    return { questionComment }
+  }
+}
